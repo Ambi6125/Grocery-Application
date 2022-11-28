b91437042740954666a2f4044926dde4d8ff9f66
Yannick <y.gorissen@student.fontys.nl>
2022-11-28 12:45:41 +0100

    Created solution and entity project with 3 main folders

---
+++ Synthesis/SynthesisEntities/Accounts/Passwords/HashAlgorithms.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Security.Cryptography;
+
+
+namespace SynthesisEntities.Passwords
+{
+    public delegate string HashAlgorithm(string salt, string password);
+
+    public static class PasswordHelper
+    {
+
+    }
+}
